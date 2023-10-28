b66b445d4ebda87862086d5ff0856ea208d1a519
raymart-vergara <raymart.furukawa@gmail.com>
2023-10-28 15:49:20 +0800

    remove white spaces

--- process/import/import_po.php
+++ process/import/import_po.php
@@ -68,7 +68,6 @@ function check_csv($file, $conn)
     }
     return $message;
 }
-
 if (isset($_POST['upload'])) {
     $csvMimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain');
 
